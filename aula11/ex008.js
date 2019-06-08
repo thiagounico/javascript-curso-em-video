6d12a5588108de28bf1fefaae6fe285e7a982dfd
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2019-06-08 08:26:10 -0300

    Adicionar aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60){  // Condição simples  
+    console(`Você ultrapassou a velocidade permitida. MULTADO`);
+}
+
+else{
+    console.log('Dirija sempre usando cinto de segurança!');
+}
